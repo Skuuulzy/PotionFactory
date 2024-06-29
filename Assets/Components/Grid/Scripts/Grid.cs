acb8f0927b120db5722e3975f536cbd23da2edba
Skuuulzy <yona.rutkowski@gmail.com>
2024-06-29 12:09:30 +0200

    Clean structure and files

---
+++ Assets/Components/Grid/Scripts/Grid.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using UnityEngine;
+using CodeMonkey.Utils;
+using System.Collections.Generic;
+
+namespace Components.Grid
+{
+    public class Grid
+    {
+        public readonly Action<int,int> OnCellValueChanged;
+
+        private readonly int _width;
+        private readonly int _height;
+        private readonly float _cellSize;
+        private readonly Vector3 _originPosition;
+        private readonly int[,] _gridArray;
+        private readonly List<Cell> _cellsList;
+
+        public Grid(int width, int height, float cellSize, Vector3 originPosition, Transform parentTransform)
+        {
+            _width = width;
+            _height = height;
+            _cellSize = cellSize;
+            _originPosition = originPosition;
+
+            _gridArray = new int[width, height];
+            _cellsList = new List<Cell>();
+            bool showDebug = true;
+            if (showDebug)
+            {
+                TextMesh[][] debugTextArray = new TextMesh[width][];
+                for (int index = 0; index < width; index++)
+                {
+                    debugTextArray[index] = new TextMesh[height];
+                }
+
+                for (int x = 0; x < _gridArray.GetLength(0); x++)
+                {
+                    for (int y = 0; y < _gridArray.GetLength(1); y++)
+                    {
+                        //Create a new cell and add it to cellLList
+                        Cell cell = new Cell(x, y, cellSize, false);
+                        _cellsList.Add(cell);
+
+                        debugTextArray[x][y] = UtilsClass.CreateWorldText(_gridArray[x, y].ToString(), parentTransform, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 30, Color.white,
+                            TextAnchor.MiddleCenter);
+                        Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                        Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+                    }
+                }
+
+                Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+
+                OnCellValueChanged += (x, y) =>
+                {
+                    debugTextArray[x][y].text = _gridArray[x, y].ToString();
+                };
+            }
+        }
+
+        #region HELPERS
+
+        public int GetWidth()
+        {
+            return _width;
+        }
+
+        public int GetHeight()
+        {
+            return _height;
+        }
+
+        public float GetCellSize()
+        {
+            return _cellSize;
+        }
+
+        public Vector3 GetWorldPosition(int x, int y)
+        {
+            return new Vector3(x, y) * _cellSize + _originPosition;
+        }
+
+        private void GetXY(Vector3 worldPosition, out int x, out int y)
+        {
+            x = Mathf.FloorToInt((worldPosition - _originPosition).x / _cellSize);
+            y = Mathf.FloorToInt((worldPosition - _originPosition).y / _cellSize);
+        }
+
+        #endregion HELPERS
+
+        #region VALUES
+
+        public void SetValue(int x, int y, int value)
+        {
+            if (x >= 0 && y >= 0 && x < _width && y < _height)
+            {
+                _gridArray[x, y] = value;
+                OnCellValueChanged?.Invoke(x,y);
+            }
+        }
+
+        public void SetValue(Vector3 worldPosition, int value)
+        {
+            int x, y;
+            GetXY(worldPosition, out x, out y);
+            SetValue(x, y, value);
+        }
+
+        public void ResetAllValue()
+        {
+            for (int x = 0; x < _gridArray.GetLength(0); x++)
+            {
+                for (int y = 0; y < _gridArray.GetLength(1); y++)
+                {
+                    SetValue(x, y, 0);
+                }
+            }
+        }
+
+        public int GetValue(int x, int y)
+        {
+            if (x >= 0 && y >= 0 && x < _width && y < _height)
+            {
+                return _gridArray[x, y];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int GetValue(Vector3 worldPosition)
+        {
+            int x, y;
+            GetXY(worldPosition, out x, out y);
+            return GetValue(x, y);
+        }
+
+        #endregion VALUES
+
+        #region CELLS
+
+        public Cell GetCellByPosition(int x, int y)
+        {
+            foreach (Cell cell in _cellsList)
+            {
+                if (cell.X == x && cell.Y == y)
+                {
+                    return cell;
+                }
+            }
+
+            return null;
+        }
+
+        public Cell GetCellByPosition(Vector3 worldPosition)
+        {
+            int x, y;
+            GetXY(worldPosition, out x, out y);
+            return GetCellByPosition(x, y);
+        }
+
+        #endregion CELLS
+    }
+}
---
+++ Assets/Components/Grid/Scripts/GridGenerator.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using CodeMonkey.Utils;
+using System.Collections.Generic;
+
+namespace Components.Grid
+{
+    public class GridGenerator : MonoBehaviour
+    {
+        private Grid _grid;
+        [SerializeField] private int _gridXValue = 64;
+        [SerializeField] private int _gridYValue = 64;
+        [SerializeField] private float _cellSize = 10;
+        [SerializeField] private Vector3 _startPosition = new(0, 0);
+
+        [SerializeField] private GameObject _objectToInstantiate;
+
+        [Header("Holders")]
+        [SerializeField] private Transform _textsHolder;
+        [SerializeField] private Transform _objectsHolder;
+        
+        
+        private bool _leftClickIsDragging;
+        private bool _rightClickIsDragging;
+        private readonly Dictionary<Cell, GameObject> _instancedObjects = new();
+
+        private void Start()
+        {
+            _grid = new Grid(_gridXValue, _gridYValue, _cellSize, _startPosition, _textsHolder);
+        }
+
+        private void Update()
+        {
+            //Right click button press
+            if (Input.GetMouseButtonDown(1))
+            {
+                _rightClickIsDragging = true;
+            }
+            //Right click button release
+            else if (Input.GetMouseButtonUp(1))
+            {
+                _rightClickIsDragging = false;
+            }
+
+            //If player press right click button => handle right click
+            if (_rightClickIsDragging)
+            {
+                HandleRightClick();
+            }
+
+            //Right click button press
+            if (Input.GetMouseButtonDown(0))
+            {
+                _leftClickIsDragging = true;
+            }
+            //Right click button release
+            else if (Input.GetMouseButtonUp(0))
+            {
+                _leftClickIsDragging = false;
+            }
+
+            //If player press left click button => handle left click
+            if (_leftClickIsDragging)
+            {
+                HandleLeftClick();
+            }
+        }
+
+        private void HandleLeftClick()
+        {
+            //Get the cell that player clicked on
+            Cell chosenCell = _grid.GetCellByPosition(UtilsClass.GetWorldPositionFromUI_Perspective());
+
+            //Check if the cell is not null and if it isn't contain a machine already
+            if (chosenCell != null && chosenCell.ContainsObject == false)
+            {
+                //Cell the value of the grid to 1 (useless but nice feedback)
+                _grid.SetValue(UtilsClass.GetWorldPositionFromUI_Perspective(), 1);
+
+                //Create a GameObject to the cell position and attached it to 3D parent Transform => need to change the game object by the machine that we want to add
+                GameObject go = Instantiate(_objectToInstantiate,
+                    _grid.GetWorldPosition(chosenCell.X, chosenCell.Y) +
+                    new Vector3(_grid.GetCellSize() / 2, _grid.GetCellSize() / 2, -_objectToInstantiate.transform.localScale.z / 2), Quaternion.identity, _objectsHolder);
+
+                //Add it to a dictionary to track it after
+                _instancedObjects.Add(chosenCell, go);
+
+                //Set the AlreadyContainsMachine bool to true
+                chosenCell.AddMachineToCell();
+            }
+        }
+
+        private void HandleRightClick()
+        {
+            //Get the cell that player clicked on
+            Cell chosenCell = _grid.GetCellByPosition(UtilsClass.GetWorldPositionFromUI_Perspective());
+
+            //Check if the cell is not null and if it contains a machine already
+            if (chosenCell != null && chosenCell.ContainsObject)
+            {
+                //Cell the value of the grid to 0 (useless but nice feedback)
+                _grid.SetValue(UtilsClass.GetWorldPositionFromUI_Perspective(), 0);
+
+                //Destroy the GameObject from the cell position
+                Destroy(_instancedObjects[chosenCell]);
+                _instancedObjects.Remove(chosenCell);
+
+                //Set the AlreadyContainsMachine bool to false
+                chosenCell.RemoveMachineFromCell();
+            }
+        }
+        
+        public void ClearGrid()
+        {
+            foreach (var cell in _instancedObjects)
+            {
+                Destroy(cell.Value);
+                cell.Key.RemoveMachineFromCell();
+            }
+
+            _grid.ResetAllValue();
+            _instancedObjects.Clear();
+        }
+    }
+}
