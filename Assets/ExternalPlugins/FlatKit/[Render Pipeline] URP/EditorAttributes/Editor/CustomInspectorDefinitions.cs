ae432c5f84af87a48a65458bb6f76688c85a239d
Skuuulzy <yona.rutkowski@gmail.com>
2025-02-28 18:37:40 +0100

    Flat kit remove 2

--- Assets/ExternalPlugins/FlatKit/[Render Pipeline] URP/EditorAttributes/Editor/CustomInspectorDefinitions.cs
+++ Assets/ExternalPlugins/FlatKit/[Render Pipeline] URP/EditorAttributes/Editor/CustomInspectorDefinitions.cs
@@ -3,14 +3,6 @@ using UnityEditor;
 
 namespace ExternPropertyAttributes.Editor {
 [CanEditMultipleObjects]
-[CustomEditor(typeof(OutlineSettings))]
-public class OutlineSettingsInspector : ExternalCustomInspector { }
-
-[CanEditMultipleObjects]
 [CustomEditor(typeof(FogSettings))]
 public class FogSettingsInspector : ExternalCustomInspector { }
-
-[CanEditMultipleObjects]
-[CustomEditor(typeof(PixelationSettings))]
-public class PixelationSettingsInspector : ExternalCustomInspector { }
 }
